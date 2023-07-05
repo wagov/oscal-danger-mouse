22550c11c9d2f8a4053e053e3bdd05a9317959e1
Adon Metcalfe <adon.metcalfe@dpc.wa.gov.au>
2023-07-06 07:48:35 +0800

    Update vite.config.ts
    relative paths for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '',
   esbuild: {
     supported: {
       'top-level-await': true //browsers can handle top-level-await features
